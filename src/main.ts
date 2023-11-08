8add070167cf75e2e54cfd888c22b3d2b61d8120
Danilo Zuluaga <48733530+cdzv@users.noreply.github.com>
2023-11-08 15:08:41 -0500

    Enable cors and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -8,6 +9,15 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Entrelazos API')
+    .setDescription('Proyecto "Misionero compra misionero"')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
   const configService = app.get(ConfigService);
   await app.listen(configService.get('port'));
 }
