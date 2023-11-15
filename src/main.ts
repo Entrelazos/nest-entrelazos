247ba11d0630154a92dd7d15243ddab34448b6af
Felipe Marin <lfelipe.marin@gmail.com>
2023-11-15 11:46:41 -0500

    allowing cors for local and cyclic

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://pear-clear-sockeye.cyclic.app',
+  ];
+  app.enableCors({
+    origin: allowedOrigins, // Replace with your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   const configService = app.get(ConfigService);
   await app.listen(configService.get('port'));
 }
