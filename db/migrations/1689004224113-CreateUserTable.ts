54d80bc2ba675002d5e31b3e1e3cb565f576e22e
Felipe Marin <lfelipe.marin@gmail.com>
2023-12-01 15:16:10 -0500

    code format

--- db/migrations/1689004224113-CreateUserTable.ts
+++ db/migrations/1689004224113-CreateUserTable.ts
@@ -1,15 +1,18 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUserTable1689004224113 implements MigrationInterface {
-    name = 'CreateUserTable1689004224113'
+  name = 'CreateUserTable1689004224113';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`address\` varchar(255) NOT NULL, \`cellphone\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`identification\` varchar(255) NOT NULL, \`is_active\` tinyint NOT NULL, \`name\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`createdAt\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`address\` varchar(255) NOT NULL, \`cellphone\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`identification\` varchar(255) NOT NULL, \`is_active\` tinyint NOT NULL, \`name\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`createdAt\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`IDX_97672ac88f789774dd47f7c8be\` ON \`users\``);
-        await queryRunner.query(`DROP TABLE \`users\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX \`IDX_97672ac88f789774dd47f7c8be\` ON \`users\``,
+    );
+    await queryRunner.query(`DROP TABLE \`users\``);
+  }
 }
