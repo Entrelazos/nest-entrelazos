a7af21c41f4ca9455016fcee7469f8605db610d8
Felipe Marin <lfelipe.marin@gmail.com>
2023-12-12 13:19:53 -0500

    removed extra enableCors

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
   const configService = app.get(ConfigService);
   await app.listen(configService.get('port'));
 }
